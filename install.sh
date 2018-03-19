f2d42457fae40de48e4202207ef4be6b38af3252
André Carvalho <andre.carvalho@soaexpert.com.br>
2018-03-19 13:44:01 -0300

    add echo to output curl commands

--- install.sh
+++ install.sh
@@ -40,21 +40,21 @@ feh --randomize --bg-fill ~/Wallpapers/*
 " > ~/.config/openbox/autostart.sh
 
 #download some wallpapers
-mkdir ~/Wallpapers && cd ~/Wallpapers
-curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-lovely-black-hole-hd-live-wallpaper-free-of-android-version-of-windows-live-wallpaper.png
-curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-best-of-the-25-best-animated-desktop-backgrounds-ideas-on-pinterest-of-windows-live-wallpaper.jpg
-curl -O http://blog.freshcrop.com/wp-content/uploads/2014/12/1-data-recovery-software.jpg
-curl -O https://images6.alphacoders.com/705/705204.jpg
-curl -O https://i.imgur.com/j6ELJse.jpg
-curl -O https://wallpapercave.com/wp/27iFUEE.jpg
-curl -O http://www.desktopas.com/files/2013/06/Black-Iron-Man-Marvel-Comics-Black-Background-1920x1080.jpg
-curl -O http://www.freejupiter.com/wp-content/uploads/2014/03/Marvel-iPhone-Wallpapers-3.jpg
-curl -O https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZxLPPbWd0BOFeny1KilSA8nFTuZFo_TLoeRX_p7Av7BiU8NDD4Q
-curl -O https://forum4d.com/wp-content/uploads/data/2018/1/21/Comics-deadpool-captain-america-marvel-wallpaper-gallery-PIC-FWP6004641.jpg
+mkdir ~/Wallpapers
+cd ~/Wallpapers
+echo curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-lovely-black-hole-hd-live-wallpaper-free-of-android-version-of-windows-live-wallpaper.png
+echo curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-best-of-the-25-best-animated-desktop-backgrounds-ideas-on-pinterest-of-windows-live-wallpaper.jpg
+echo curl -O http://blog.freshcrop.com/wp-content/uploads/2014/12/1-data-recovery-software.jpg
+echo curl -O https://images6.alphacoders.com/705/705204.jpg
+echo curl -O https://i.imgur.com/j6ELJse.jpg
+echo curl -O https://wallpapercave.com/wp/27iFUEE.jpg
+echo curl -O http://www.desktopas.com/files/2013/06/Black-Iron-Man-Marvel-Comics-Black-Background-1920x1080.jpg
+echo curl -O http://www.freejupiter.com/wp-content/uploads/2014/03/Marvel-iPhone-Wallpapers-3.jpg
+echo curl -O https://forum4d.com/wp-content/uploads/data/2018/1/21/Comics-deadpool-captain-america-marvel-wallpaper-gallery-PIC-FWP6004641.jpg
 
 #download menu.xml and rc.xml from github
-curl -O https://github.com/aoc85/personal_openbox_config/blob/master/menu.xml > ~/.config/openbox/menu.xml
-curl -O https://github.com/aoc85/personal_openbox_config/blob/master/rc.xml > ~/.config/openbox/rc.xml
+echo curl -O https://raw.githubusercontent.com/aoc85/personal_openbox_config/master/menu.xml > ~/.config/openbox/menu.xml
+echo curl -O https://raw.githubusercontent.com/aoc85/personal_openbox_config/master/rc.xml > ~/.config/openbox/rc.xml
 
 
 #remove unused files
