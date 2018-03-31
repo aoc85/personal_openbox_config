1ad77a3a9e0e276d7dd646fa3669f6529aaa6859
André Carvalho <andre.carvalho@soaexpert.com.br>
2018-03-31 15:26:15 -0300

    sync files with in use config

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#tint2 autostart
+tint2 &
+#feh autostart
+feh &
+feh --randomize --bg-fill ~/Wallpapers/*
+
--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ echo ALSO CHROME, OPERA AND FIREFOX BROWSERS, VIM, VSCODE and ATOM WILL BE INSTA
 echo ""
 echo "Starting installation"
 #install curl
-sudo apt-get update
-sudo apt-get install curl
+#sudo apt-get update
+#sudo apt-get install curl
 
 #add google-chrome repository
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 #add opera repository
-sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
-wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+#sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
+#wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
 
 #add vscode repository
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+#sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+#sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 #install openbox, feh
 sudo apt-get update
@@ -42,17 +42,17 @@ feh --randomize --bg-fill ~/Wallpapers/*
 " > ~/.config/openbox/autostart.sh
 
 #download some wallpapers
-mkdir ~/Wallpapers
-cd ~/Wallpapers
-curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-lovely-black-hole-hd-live-wallpaper-free-of-android-version-of-windows-live-wallpaper.png
-curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-best-of-the-25-best-animated-desktop-backgrounds-ideas-on-pinterest-of-windows-live-wallpaper.jpg
-curl -O http://blog.freshcrop.com/wp-content/uploads/2014/12/1-data-recovery-software.jpg
-curl -O https://images6.alphacoders.com/705/705204.jpg
-curl -O https://i.imgur.com/j6ELJse.jpg
-curl -O https://wallpapercave.com/wp/27iFUEE.jpg
-curl -O http://www.desktopas.com/files/2013/06/Black-Iron-Man-Marvel-Comics-Black-Background-1920x1080.jpg
-curl -O http://www.freejupiter.com/wp-content/uploads/2014/03/Marvel-iPhone-Wallpapers-3.jpg
-curl -O https://forum4d.com/wp-content/uploads/data/2018/1/21/Comics-deadpool-captain-america-marvel-wallpaper-gallery-PIC-FWP6004641.jpg
+#mkdir ~/Wallpapers
+#cd ~/Wallpapers
+#curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-lovely-black-hole-hd-live-wallpaper-free-of-android-version-of-windows-live-wallpaper.png
+#curl -O http://ieeesablog.com/wp-content/uploads/2018/03/windows-live-wallpaper-best-of-the-25-best-animated-desktop-backgrounds-ideas-on-pinterest-of-windows-live-wallpaper.jpg
+#curl -O http://blog.freshcrop.com/wp-content/uploads/2014/12/1-data-recovery-software.jpg
+#curl -O https://images6.alphacoders.com/705/705204.jpg
+#curl -O https://i.imgur.com/j6ELJse.jpg
+#curl -O https://wallpapercave.com/wp/27iFUEE.jpg
+#curl -O http://www.desktopas.com/files/2013/06/Black-Iron-Man-Marvel-Comics-Black-Background-1920x1080.jpg
+#curl -O http://www.freejupiter.com/wp-content/uploads/2014/03/Marvel-iPhone-Wallpapers-3.jpg
+#curl -O https://forum4d.com/wp-content/uploads/data/2018/1/21/Comics-deadpool-captain-america-marvel-wallpaper-gallery-PIC-FWP6004641.jpg
 
 #download menu.xml and rc.xml from github
 curl https://raw.githubusercontent.com/aoc85/personal_openbox_config/master/menu.xml > ~/.config/openbox/menu.xml
